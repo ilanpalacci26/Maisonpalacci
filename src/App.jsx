d7151312a72b95254a59395191f9feec4a8b55c2
ilanpalacci26 <62207694+ilanpalacci26@users.noreply.github.com>
2025-08-28 22:18:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import React, { useMemo, useState } from "react";
 // Données du site
 // =====================
 const CATALOG_IMAGES = [
-  { src: "https://drive.google.com/file/d/1KzkJVF0HfY7VmzG0UKMSoJQvCI7sUlVc/view?usp=drivesdk", alt: "purple" },
-  { src: "https://drive.google.com/file/d/1TVh49hp2ObBidXjZBt1OKk3WegVTj8Gp/view?usp=drivesdk", alt: "purple" },
-  { src: "https://images.unsplash.com/photo-1542060748-10c28b62716d?q=80&w=1600&auto=format&fit=crop", alt: "Table de coupe couture" },
+  { src: "galerie/dream1.jpeg", alt: "purple" },
+  { src: "galerie/dream2.jpeg", alt: "purple" },
+  { src: "galerie/purple1.jpeg", alt: "Table de coupe couture" },
   { src: "https://images.unsplash.com/photo-1520975842173-8b6835f2f1b4?q=80&w=1600&auto=format&fit=crop", alt: "Broderie main haute couture" },
   { src: "https://images.unsplash.com/photo-1542060747-8b8a9b5fbd69?q=80&w=1600&auto=format&fit=crop", alt: "Épingles et patron papier" },
   { src: "https://images.unsplash.com/photo-1520975962035-1282d8f7f1c1?q=80&w=1600&auto=format&fit=crop", alt: "Dentelle et perles" },
